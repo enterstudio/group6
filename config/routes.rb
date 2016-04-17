8f93119d14021675f3061f945449b70dbced9dc7
Chris Ward <chriswhward@gmail.com>
2016-04-17 19:14:23 +0200

    Adding Bootstrap and removing defunct files

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  # root 'welcome#index'
+  root 'welcome#index'
 
 
     get "articles/index" => 'articles#index' 
